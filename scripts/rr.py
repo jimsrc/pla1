5f56cfc7840e7b6af241783142444d54bc99c036
jimsrc <jimmy.ilws@gmail.com>
2016-05-09 13:20:31 -0300

    modified 'rr.py' in order to: 1) capture the full sequence of step-sizes in the simulation;   2) build histograms of these step-sizes

--- scripts/rr.py
+++ scripts/rr.py
@@ -14,13 +14,26 @@ from os.path import isfile, isdir
 import os
 
 
+"""
+(Bo = 5nT; omega=omega_{rel})
+Ek/eV   Rigidity/V   Rl/AU          beta
+1e6     4.33306E+07  1.929785E-04   4.613215E-02
+1e7     1.37352E+08  6.117125E-04   1.448440E-01
+1e8     4.44583E+08  1.980009E-03   4.281955E-01
+1e9     1.69604E+09  7.553521E-03   8.750257E-01
+1e10    1.0898E+10   4.853544E-02   9.963142E-01
+"""
 #--- set B-turbulence model
 pd['n_modos']    = 128
 pd['lambda_min'] = ((5e-5)*AU_in_cm)
 #--- corregimos input
-psim['atol']     = 1e-5 #3.3e-6 #1e-6 #1e-5
+psim['rigidity'] = 4.33306E+07
+psim['tmax']     = 4e4 #0.3e4 #4e4
+rl = cw.calc_Rlarmor(psim['rigidity'],pd['Bo']) #[cm]
+eps_o = 3.3e-6 #3.33e-5 #4e-5 # ratio: (error-step)/(lambda_min)
+psim['atol']     = pd['lambda_min']*eps_o/rl
 psim['rtol']     = 0.0 #1e-6
-psim['tmax']     = 1e4 #0.3e4 #4e4
+#---------------------------
 #--- output
 po = {}
 po.update(psim)
@@ -37,6 +50,9 @@ m.set_Bmodel(pdict=pd, nB=nB)
 comm        = MPI.COMM_WORLD
 rank        = comm.Get_rank()   # proc rank
 wsize       = comm.Get_size()   # number of proc
+#---------------------------
+if rank==0:
+    print " simul parameters\n", psim
 
 pla_bd  = ff.equi_bounds(0, mu.size-1, wsize) # bounds
 plas    = np.arange(pla_bd[rank], pla_bd[rank+1]) # plas for each proc
@@ -68,12 +84,27 @@ for npla in plas: #[25:]:
         pause(0.1)
         fo = h5(fname_out, 'r+') # read again
 
-    print " [r:%d] now i'll write" % rank
-
     fo['mine'] = 1    # file ocupado
+    print " [r:%d] now i'll write" % rank
     dpath = 'pla%03d/' % npla
     ff.SaveToFile(m, dpath=dpath, f=fo)
-    pause(0.1)
+    #--- histos of step-size
+    st      = m.step_save # shape: (2,Odeint::MAXSTP)
+    st_tot  = st[0,:][st[0,:]!=0] # filter-out zero values
+    st_part = st[1,:][st[1,:]!=0] # ""
+    #st     = st[0,:][st!=0]   # filter-out zero values
+    nbin = 1000
+    h    = np.histogram2d(st_tot, st_part, 
+                bins=[nbin, nbin], 
+                normed=False)
+    HStp = h[0].T # filter-out zero values
+    bins_StepTot  = 0.5*(h[1][:-1] + h[1][1:])
+    bins_StepPart = 0.5*(h[2][:-1] + h[2][1:])
+    fo[dpath+'HistStep/HStep'] = HStp.sum(axis=0)
+    #fo[dpath+'HistStep/bins_StepTot']  = bins_StepTot
+    fo[dpath+'HistStep/bins_StepPart'] = bins_StepPart
+    fo[dpath+'HistStep/nbin']          = nbin
+    pause(0.3)
     fo.pop('mine')    # libera file
 
     fo.close()
