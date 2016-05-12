35f577720ff742f7beae5f67caae88c1304ca8aa
jimsrc <jimmy.ilws@gmail.com>
2016-05-12 02:05:06 -0300

    class to unify all histograms (of step-sizes) of the simulations with several particles and ONE B-realization (produced by rr.py)

--- scripts/shared/funcs.py
+++ scripts/shared/funcs.py
@@ -4,6 +4,50 @@ from h5py import File as h5
 from os.path import isfile, isdir
 import numpy as np
 from pylab import pause
+from numpy import min, max
+
+
+
+class Hmgr:
+    """ class to unify all histograms of 
+    step-sizes of every simulation with 
+    several particles and one B-realization """
+    def __init__(self, file, nbin=1000):
+        self.f = file
+        bmin, bmax = self.get_hstep_extremes()
+        self.dbin = dbin = (bmax-bmin)/nbin
+        self.hbin = np.arange(bmin+0.5*dbin, bmax, dbin)
+        self.nbin = nbin
+        # build histogram bounds
+        self.hbd = hbd = np.zeros(self.nbin+1)
+        hbd[:-1] = self.hbin - 0.5*self.dbin
+        hbd[-1] = self.hbin[-1] + 0.5*self.dbin
+        # initialize histogram in zero counts
+        self.h = np.zeros(nbin)
+
+    def get_hstep_extremes(self):
+        """ Obtain the max && min of all the histogram domains """
+        PNAMES  = self.f.keys()
+        self.Np = len(PNAMES)
+        self.bmin = 1e31
+        self.bmax = 0.0
+        for pnm in PNAMES:
+            hx = self.f[pnm+'/HistStep/bins_StepPart'].value
+            self.bmin = min(self.bmin, hx[0])
+            self.bmax = max(self.bmax, hx[-1])
+        return self.bmin, self.bmax
+
+    def pile_to_hist(self, hx, hin):
+        hbd = self.hbd
+        assert (hx[0]>=hbd[0]) & (hx[-1]<=hbd[-1]), \
+            " RIDICULOUS ***ERROR*** IN OUR BOUNDARIES!!"
+
+        for i in range(hx.size):
+            #i = 0
+            cc = (hx[i]>hbd[:-1]) & (hx[i]<hbd[1:])
+            indx = find(cc)
+            self.h[indx] += hin[i]
+
 
 
 def load_traj(fname):
