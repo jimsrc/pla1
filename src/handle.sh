e49277647e43fcff5f80e7a920c745c4f8b50dea
jimsrc <jimmy.ilws@gmail.com>
2016-05-31 21:01:32 -0300

    routine to sync file from other repo

--- src/handle.sh
+++ src/handle.sh
@@ -18,25 +18,39 @@ function get_sources(){
     echo "${l[@]}"
 }
 
+
+function check_diffs(){
+    echo -e "\n ---> comparing with:\n " ${dir_comp}"\n"
+    for fname in ${fname_list[@]}; do
+        fname2=${dir_comp}/$fname
+        diff_result=$(diff $fname $fname2) # compare text
+        #if cmp -s $fname $fname2; then
+        if [ "$diff_result" == "" ]; then
+            cc="\033[32m" # synced
+            ok=1
+        else
+            cc="\033[31m" # not synced
+            ok=0
+        fi
+        echo -e $cc "--> [$ok] $fname"
+    done
+}
+
 #get_sources
 fname_list=$(get_sources)
 echo $fname_list
-#echo $ll
-
 dir_comp=$PLAS/src # directorio con el q vamos a comparar
-echo -e "\n ---> comparing with:\n " ${dir_comp}"\n"
-for fname in ${fname_list[@]}; do
-    fname2=${dir_comp}/$fname
-    diff_result=$(diff $fname $fname2) # compare text
-    #if cmp -s $fname $fname2; then
-    if [ "$diff_result" == "" ]; then
-        cc="\033[32m" # synced
-        ok=1
-    else
-        cc="\033[31m" # not synced
-        ok=0
-    fi
-    echo -e $cc "--> [$ok] $fname"
-done
 
-#echo ${fname_list[0]}
+#--------------------- execute
+if [[ -n $1 ]]; then
+    if [[ "$1"=="sync" ]]; then
+        # copy file from ${dir_comp} to $PWD
+        /bin/cp -p ${dir_comp}/$2 $PWD/.
+        echo " ----> SYNCED WITH: " ${dir_comp}/$2
+    fi
+fi
+
+check_diffs
+#-----------------------------
+
+#EOF
