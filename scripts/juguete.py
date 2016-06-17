ce59cf067091e2b6d539955bf154470d25673c20
jimsrc <jimmy.ilws@gmail.com>
2016-06-17 17:14:17 -0300

    setting to analyze outputs with eps=1e-4 (with different Nm_slab & Nm_2d)

--- scripts/juguete.py
+++ scripts/juguete.py
@@ -7,15 +7,16 @@ import shared.funcs as sf
 ps = {
 'dir_src'   : '%s/out' % os.environ['PLA1'],
 'dir_dst'   : '%s/figs' % os.environ['PLA1'],
-'id'        : (1, 2, 3, 4, 5, 6, 7, 8),
-'Nm_slab'   : (64, 64, 64, 64, 256, 64, 64, 64),
-'Nm_2d'     : (64, 128, 64, 64, 256, 128, 64, 64),
-'eps_o'     : (3.33e-6, 1e-5, 3.33e-5, 4.64e-5, 1e-4, 3.33e-4, 4.64e-4, 1e-3),
+'id'        : (9, 10, 11, 12, 5),
+'Nm_slab'   : (64, 64, 128, 128, 256),
+'Nm_2d'     : (64, 128, 128, 256, 256),
+#'eps_o'     : (3.33e-6, 1e-5, 3.33e-5, 4.64e-5, 1e-4, 3.33e-4, 4.64e-4, 1e-3),
 'label'     : [], #('uno', 'dos', 'tres'),
 }
 
-for e in ps['eps_o']:
-    ps['label'] += [ '$\epsilon: %1.2e$'%e ]
+for Nms, Nm2d in zip(ps['Nm_slab'], ps['Nm_2d']):
+    #ps['label'] += [ '$\epsilon: %1.2e$'%e ]
+    ps['label'] += [ '$Nm^{slab}, Nm^{2d}: %d, %d$' % (Nms, Nm2d) ]
 
 gp = sf.GralPlot(ps=ps, check=('eps_o',), check_all=True)
 #gp = sf.GralPlot(ps=ps, check=None, check_all=False)
