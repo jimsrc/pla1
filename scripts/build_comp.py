1f9689b482b080cd0431253b0783a2c9127efef5
jimsrc <jimmy.ilws@gmail.com>
2016-12-16 00:10:18 -0300

    profiles of lperp(t) in physical time-scale

--- scripts/build_comp.py
+++ scripts/build_comp.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env ipython
 # -*- coding: utf-8 -*-
 import numpy as np
-import h5py
+import h5py, argparse
 from pylab import figure, close
 import shared.funcs_post as sfp
 from shared.funcs import Bo_parker, Lc_memilia, calc_Rlarmor
+
+# args
+parser = argparse.ArgumentParser(
+formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument(
+'-fig', '--figname', 
+type=str,
+help='figname.',
+)
+pa = parser.parse_args()
 
 ro = 1 # [AU]
 #--- seeds for fit
@@ -37,13 +48,31 @@ for id in fs.keys():
     tadim = o['tadim'] # [1]
     lfit[id] = {}
     Lc_adim = f['psim/Lc_slab'].value # [1]
+    ss.update({(id,'t'): tadim/omg}) # [seg]
     for nm in ('xx','yy','zz'):
         mfp = 3.*o['k'+nm]/Lc_adim # [1] normalized units
         lfit[id][nm] = sfp.fit_mfp(tadim, mfp, t_decr=tdecr, **(seeds[nm]))
-
         ss.update({(id,nm): mfp,})
 
 #--- figs
+fig = figure(1, figsize=(6,4))
+ax  = fig.add_subplot(111)
 
+#id=1
+for id in (1,2):
+    lperp = .5*(ss[(id,'xx')] + ss[(id,'yy')]) # [1]
+    time  = ss[(id,'t')]  # [sec]
+    lperp_fit = 0.5*(lfit[id]['xx'] + lfit[id]['yy'])
+    ax.plot(time/3600., lperp/lperp_fit, '-o', ms=3, label='$R=%2.2e GV$'%(fs[id][1]/1e9))
+
+ax.set_xlabel('time [hr]')
+ax.set_ylabel('$\lambda_\perp / \lambda_{\perp O}$ [1]')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.grid(True)
+ax.legend(loc='best')
+
+fig.savefig(pa.figname, dpi=135, bbox_inches='tight')
+close(fig)
 
 #EOF
