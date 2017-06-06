22689b112aff0134ae72ed0ba81fa01f5e137b0f
jimarch <jimmy.ilws@gmail.com>
2017-06-06 13:30:52 -0300

    compilation fixes

--- Bparker/setup.py
+++ Bparker/setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env ipython
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import numpy
 
 modname = 'Bparker'
 
@@ -10,6 +11,7 @@ ext = Extension(
         "%s.pyx" % modname, 
     ],
     language="c++",
+    include_dirs=[numpy.get_include()],
     #--- for debugging with 'gdb python'
     #extra_compile_args = ['-g'],
     #extra_link_args = ['-g'],
