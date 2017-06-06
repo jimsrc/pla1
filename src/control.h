b03e8fe54766db31b3e694da3b2a61a1afb7c640
jimarch <jimmy.ilws@gmail.com>
2017-06-06 15:04:59 -0300

    bash script to generate Cython macros from control.h :D :D This is called my the makefile.

--- src/control.h
+++ src/control.h
@@ -15,7 +15,7 @@
 #define NVAR    (6)
 
 //--------------------------------------CTES UNIVERSALES
-#define SIM_MAXSTP      (3000*50000) // max nuber of steps in Odeint
+#define SIM_MAXSTP      (900*50000) // max nuber of steps in Odeint
 #define clight          (3.0*1e10)              // [cm/s]
 #define AU_in_cm        (1.5e13)                // [cm]
 #define nT_in_G         (1.0*1e-5)              // [1G=1e5nT]
