81b811d713798f9ccb8ad33dd826b8e72896f57e
jimlamp <jimmy.ilws@gmail.com>
2017-12-15 23:51:02 -0300

    r=0.5; NmS,Nm2d=256,256; eps=4.64e-4

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -38,8 +38,8 @@ echo ""
 mpirun $NPROCS $EXEC -- \
     --fname_out $ofname \
     --ro $ro \
-    --Nm_slab 128 \
-    --Nm_2d 128 \
+    --Nm_slab 256 \
+    --Nm_2d 256 \
     --tmax 4e4 \
     --eps 4.64e-4 \
     --sigma 0.3 \
