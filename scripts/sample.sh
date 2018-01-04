c8a8f5d0c49645c6ae71671f02ac4fec7bf71484
jimlamp <jimmy.ilws@gmail.com>
2018-01-04 18:31:17 -0300

    r=0.9; NmS,Nm2d=128,128; eps=4.64e-5 && collecting trails

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -2,7 +2,7 @@
 
 export REPO=$HOME/pla1__Lc          # Git work-tree on lamp.iafe.uba.ar
 EXEC=$REPO/scripts/rr_hydra.py
-NPROCS="-np 7"                 # number of processors
+NPROCS="-np 24"                 # number of processors
 
 
 CONDAENV=work2      # conda environment where we work
@@ -17,7 +17,7 @@ else
     echo -e "\n [-] dirty Git repo. Commit input changes && commit!\n" && exit 1
 fi
 
-ro="0.50"  #$(printf "%.2f" 0.5)                            # [AU] heliodistance
+ro="0.90"  #$(printf "%.2f" 0.5)                            # [AU] heliodistance
 ofname="$REPO/out/newLc/r_${ro}__$(git rev-parse HEAD).h5"  # output HDF5
 
 # check if there's already a .h5 with this name!
@@ -41,7 +41,7 @@ mpirun $NPROCS $EXEC -- \
     --Nm_slab 128 \
     --Nm_2d 128 \
     --tmax 4e4 \
-    --eps 1e-4 \
+    --eps 4.64e-5 \
     --sigma 0.3 \
     --Nph 8 \
     --Nth 16 \
